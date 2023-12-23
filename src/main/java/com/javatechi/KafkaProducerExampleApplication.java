3e782887dcc5c24f8e879ad5eadd54c494597dee
Anurag Shukla <anuragshukla0405@gmail.com>
2023-12-23 13:15:19 -0600

    add one sop

--- src/main/java/com/javatechi/KafkaProducerExampleApplication.java
+++ src/main/java/com/javatechi/KafkaProducerExampleApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KafkaProducerExampleApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("KafkaProducerExampleApplication initiated");
 		SpringApplication.run(KafkaProducerExampleApplication.class, args);
 	}
 
